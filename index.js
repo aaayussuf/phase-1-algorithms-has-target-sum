405f4d323bc702225d9ece4e2ec9c3edffa7172c
Abdullahi Abdikadir <aabdikadir849@gmail.com>
2025-01-01 23:27:38 +0300

    Complete hasTargetSum function with tests

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
+// hasTargetSum.js
+
+/**
+ * Checks if any two numbers in the array add up to the target sum.
+ * @param {number[]} array - An array of numbers.
+ * @param {number} target - The target sum to check for.
+ * @returns {boolean} - True if any two numbers add up to the target sum, false otherwise.
+ */
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+
+  for (const num of array) {
+    const complement = target - num;
+
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+
+    seenNumbers.add(num);
+  }
+
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
+// Export the function
 module.exports = hasTargetSum;
